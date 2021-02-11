6ccfbbdf9ffd51253f69f603a78c782f48939397
Megan Morris <megan.morris@alumni.stonybrook.edu>
2021-02-11 17:27:54 -0500

    M2 D4 insert, update, delete exercise and also lecture notes

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/constraints-and-transactions-exercises.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/constraints-and-transactions-exercises.sql
@@ -2,25 +2,92 @@
 -- The following changes are applied to the "dvdstore" database.**
 
 -- 1. Add actors, Hampton Avenue, and Lisa Byway to the actor table.
+SELECT *
+FROM actor
+WHERE first_name = 'Lisa' AND last_name = 'Byway' OR first_name = 'Hampton' AND last_name = 'Avenue';
+
+INSERT INTO actor(first_name, last_name)
+VALUES ('Hampton', 'Avenue');
+
+INSERT INTO actor(first_name, last_name)
+VALUES ('Lisa', 'Byway');
+
 
 -- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in
 -- ancient Greece", to the film table. The movie was released in 2008 in English.
 -- Since its an epic, the run length is 3hrs and 18mins. There are no special
 -- features, the film speaks for itself, and doesn't need any gimmicks.
 
+INSERT INTO film(title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating)
+VALUES('EUCLIDEAN PI', 'The epic story of Euclid as a pizza delivery boy in ancient Greece', 2008, 1, 6, 7.99, 198, 25.99, 'PG-13');
+
+SELECT *
+FROM film
+WHERE title = 'EUCLIDEAN PI';
+
+
 -- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly
 -- overprotective mother, in the film, "Euclidean PI". Add them to the film.
+INSERT INTO film_actor(actor_id, film_id)
+VALUES (209,1001);
+
+INSERT INTO film_actor(actor_id, film_id)
+VALUES (210,1001);
+
+SELECT *
+FROM film_actor
+WHERE film_id = 1001;
+
 
 -- 4. Add Mathmagical to the category table.
 
+INSERT INTO category(name)
+VALUES('Mathmagical');
+
+SELECT *
+FROM category
+WHERE name = 'Mathmagical';
+
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI",
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
+
+SELECT film_id FROM film WHERE film.title ILIKE 'Young Language';
+
+INSERT INTO film_category(film_id, category_id) VALUES
+        (1001, 17),
+        (274, 17),
+        (494, 17),
+        (714, 17),
+        (996, 17);
+
+SELECT *
+FROM film_category
+WHERE category_id = 17;
+
 
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films
 -- accordingly.
 -- (5 rows affected)
 
+UPDATE film
+SET rating = 'G'
+FROM film_category
+WHERE film_category.film_id = film.film_id AND film_category.category_id = 17; 
+
+SELECT *
+FROM film
+JOIN film_category ON film_category.film_id = film.film_id
+WHERE film_category.category_id = 17;
+
 -- 7. Add a copy of "Euclidean PI" to all the stores.
+
+INSERT INTO inventory(film_id, store_id) VALUES
+        (1001, 1),
+        (1001, 2);
+        
+SELECT *
+FROM inventory
+WHERE film_id = 1001;
 
 -- 8. The Feds have stepped in and have impounded all copies of the pirated film,
 -- "Euclidean PI". The film has been seized from all stores, and needs to be
@@ -28,19 +95,74 @@
 -- (Did it succeed? Why?)
 -- <YOUR ANSWER HERE>
 
+DELETE FROM film
+WHERE title = 'EUCLIDEAN PI';
+
+--It did not succeed because this would violate the foreign key constraint - that helps us ensure our data is consisent throughout all of our tables
+--This film is still referenced from table film_actor.
+
 -- 9. Delete Mathmagical from the category table.
 -- (Did it succeed? Why?)
 -- <YOUR ANSWER HERE>
 
--- 10. Delete all links to Mathmagical in the film_category tale.
+DELETE FROM category
+WHERE name = 'Mathmagical';
+
+--This did not succeed for the same reason as above! This category id 17 is still referenced from the table film_category
+
+-- 10. Delete all links to Mathmagical in the film_category table.
 -- (Did it succeed? Why?)
 -- <YOUR ANSWER HERE>
+DELETE FROM film_category
+WHERE category_id = 17;
+
+--This succeeded because it was the end of the line, there are no tables that draw that info directly from film_category, the info is being drawn from the film table
 
 -- 11. Retry deleting Mathmagical from the category table, followed by retrying
 -- to delete "Euclidean PI".
 -- (Did either deletes succeed? Why?)
 -- <YOUR ANSWER HERE>
 
+--Deleting Mathmagical from the category table was successful. This could now be deleted because film_category no longer references it as a foreign key.
+--Euclidean PI - deleting that from film table was NOT successful. Not sucessful because there are still other tables that reference this as a foreign key.
+
 -- 12. Check database metadata to determine all constraints of the film id, and
 -- describe any remaining adjustments needed before the film "Euclidean PI" can
 -- be removed from the film table.
+
+--Constraints of film_id:
+--1) film_actor_film_id_fkey
+--2) film_category_film_id_fkey
+--3) inventory_film_id_fkey
+
+--Euclidean PI can be removed from the database AFTER the following takes place:
+        --1) All tables with foreign keys referencing this primary key have that foreign key deleted
+                --film_id is currently still on: film_actor, category_film, and inventory tables.
+                -- After deleting those foreign keys on tables mentioned above, then Euclidean PI can be removed from the film table
+
+
+
+select kcu.table_schema || '.' ||kcu.table_name as foreign_table,
+'>-' as rel,
+rel_tco.table_schema || '.' || rel_tco.table_name as primary_table,
+string_agg(kcu.column_name, ', ') as fk_columns,
+kcu.constraint_name
+from information_schema.table_constraints tco
+join information_schema.key_column_usage kcu
+on tco.constraint_schema = kcu.constraint_schema
+and tco.constraint_name = kcu.constraint_name
+join information_schema.referential_constraints rco
+on tco.constraint_schema = rco.constraint_schema
+and tco.constraint_name = rco.constraint_name
+join information_schema.table_constraints rel_tco
+on rco.unique_constraint_schema = rel_tco.constraint_schema
+and rco.unique_constraint_name = rel_tco.constraint_name
+where tco.constraint_type = 'FOREIGN KEY'
+group by kcu.table_schema,
+kcu.table_name,
+rel_tco.table_name,
+rel_tco.table_schema,
+kcu.constraint_name
+order by kcu.table_schema,
+kcu.table_name;
+
--- module-2/04_INSERT_UPDATE_and_DELETE/lecture-student/lecture-postgresql.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/lecture-student/lecture-postgresql.sql
@@ -1,38 +1,103 @@
 -- INSERT
 
 -- 1. Add Klingon as a spoken language in the USA
+INSERT INTO countrylanguage(countrycode, language, isofficial, percentage) --if nullable you don't have to have it
+VALUES ('USA', 'Klingon', false, 5);
+
+SELECT * --just to check it
+FROM countrylanguage
+WHERE countrycode = 'USA';
+
 -- 2. Add Klingon as a spoken language in Great Britain (GBR)
+INSERT INTO countrylanguage(countrycode, language, isofficial, percentage) 
+VALUES ('GBR', 'Klingon', false, 5);
+
+SELECT * --just to check it
+FROM countrylanguage
+WHERE countrycode = 'GBR';
 
 
 -- UPDATE
+-- UPDATE table_name
+-- SET column = value
+-- WHERE column = value
 
 -- 1. Update the capital of the USA to Houston
--- 2. Update the capital of the USA to Washington DC and the head of state
+SELECT id
+FROM city
+WHERE name = 'Houston';
 
+UPDATE country
+SET capital = 3796
+--SELECT * FROM country
+WHERE code = 'USA';
+
+--checking further to see it displayed nicely
+SELECT country.code, city.name
+FROM country
+JOIN city ON country.capital = city.id
+WHERE code = 'USA';
+
+--also can use subqueries to accomplish the same thing
+UPDATE country
+SET capital = (SELECT id FROM city WHERE name = 'Houston')
+WHERE code = 'USA';
+
+
+-- 2. Update the capital of the USA to Washington DC and the head of state
+-- you can update two items at the same time just use a comma to separate
+UPDATE country
+SET capital = (SELECT id FROM city WHERE name = 'Washington'),
+    headofstate = 'Elon Musk'
+WHERE code= 'USA';
 
 -- DELETE
+-- DELETE FROM table_name
+--WHERE column = value
+
 
 -- 1. Delete English as a spoken language in the USA
--- 2. Delete all occurrences of the Klingon language 
+DELETE 
+--SELECT *
+FROM countrylanguage
+WHERE language = 'English' AND countrycode = 'USA';
 
+
+-- 2. Delete all occurrences of the Klingon language 
+DELETE
+--SELECT *
+FROM countrylanguage
+WHERE language = 'Klingon';
 
 -- REFERENTIAL INTEGRITY
 
 -- 1. Try just adding Elvish to the country language table.
+INSERT INTO countrylanguage(language)
+VALUES ('Elvish'); --null value in countrycode violates not-null constraint
 
 -- 2. Try inserting English as a spoken language in the country ZZZ. What happens?
+INSERT INTO countrylanguage(countrycode, language, isofficial, percentage)
+VALUES ('ZZZ', 'English', false, 5); --violates foreign key constraint (you can't insert a language into a country that doesn't exist)
 
 -- 3. Try deleting the country USA. What happens?
+DELETE FROM country
+WHERE code = 'USA'; --not allowed to delete from country if that's going to cause another table to have a foreign key issure
 
 
 -- CONSTRAINTS
 
 -- 1. Try inserting English as a spoken language in the USA
+INSERT INTO countrylanguage(language, isOfficial, percentage, countrycode)
+VALUES ('English', false, 90, 'USA');
 
 -- 2. Try again. What happens?
+INSERT INTO countrylanguage(language, isOfficial, percentage, countrycode)
+VALUES ('English', false, 90, 'USA'); --you can't duplicate a primary key
 
 -- 3. Let's relocate the USA to the continent - 'Outer Space'
-
+UPDATE country
+SET continent = 'Outer Space'
+WHERE code = 'USA'; --this constraint checks to make sure that the value is acceptable to put in
 
 -- How to view all of the constraints
 
@@ -42,10 +107,72 @@ INNER JOIN pg_catalog.pg_class ON pg_class.oid = pg_constraint.conrelid
 INNER JOIN pg_catalog.pg_namespace ON pg_namespace.oid = connamespace;
 
 
+--GET ALL FOREIGN KEYS
+select kcu.table_schema || '.' ||kcu.table_name as foreign_table,
+'>-' as rel,
+rel_tco.table_schema || '.' || rel_tco.table_name as primary_table,
+string_agg(kcu.column_name, ', ') as fk_columns,
+kcu.constraint_name
+from information_schema.table_constraints tco
+join information_schema.key_column_usage kcu
+on tco.constraint_schema = kcu.constraint_schema
+and tco.constraint_name = kcu.constraint_name
+join information_schema.referential_constraints rco
+on tco.constraint_schema = rco.constraint_schema
+and tco.constraint_name = rco.constraint_name
+join information_schema.table_constraints rel_tco
+on rco.unique_constraint_schema = rel_tco.constraint_schema
+and rco.unique_constraint_name = rel_tco.constraint_name
+where tco.constraint_type = 'FOREIGN KEY'
+group by kcu.table_schema,
+kcu.table_name,
+rel_tco.table_name,
+rel_tco.table_schema,
+kcu.constraint_name
+order by kcu.table_schema,
+kcu.table_name;
+
+
 -- TRANSACTIONS
 
 -- 1. Try deleting all of the rows from the country language table and roll it back.
+START TRANSACTION;
+SELECT *
+FROM countrylanguage;
+
+DELETE 
+FROM countrylanguage;
+
+SELECT *
+FROM countrylanguage;
+
+ROLLBACK;
+--or normally you can commmit
+
 
 -- 2. Try updating all of the cities to be in the USA and roll it back
 
+START TRANSACTION;
+
+UPDATE city
+SET countrycode = 'USA';
+
+SELECT * FROM city;
+
+ROLLBACK;
+
 -- 3. Demonstrate two different SQL connections trying to access the same table where one happens to be inside of a transaction but hasn't committed yet.
+
+START TRANSACTION;
+
+UPDATE countrylanguage
+SET percentage = 60
+WHERE language = 'English' AND countrycode = 'USA';
+--started it haven't finished it
+--now have the file open in a diff window/program updating the same set
+--running the first one, it hasn't finished
+--you can't execute the query in the other file while the transactio is open
+--keep your transactions small and atomic
+
+
+
