228269866aaddc07d0a67fa48103bec7c7b2b901
Megan Morris <megan.morris@alumni.stonybrook.edu>
2021-03-18 16:26:45 -0400

    Event Handling exercises

--- module-3/09_Event_Handling/exercise-student/shopping-list/js/shopping-list.js
+++ module-3/09_Event_Handling/exercise-student/shopping-list/js/shopping-list.js
@@ -2,14 +2,14 @@ let allItemsIncomplete = true;
 const pageTitle = 'My Shopping List';
 const groceries = [
   { id: 1, name: 'Oatmeal', completed: false },
-  { id: 2, name: 'Milk', completed: false },
-  { id: 3, name: 'Banana', completed: false },
+  { id: 2, name: 'Almond Milk', completed: false },
+  { id: 3, name: 'Papaya', completed: false },
   { id: 4, name: 'Strawberries', completed: false },
-  { id: 5, name: 'Lunch Meat', completed: false },
-  { id: 6, name: 'Bread', completed: false },
+  { id: 5, name: 'Chicken Sausage', completed: false },
+  { id: 6, name: 'Eggs', completed: false },
   { id: 7, name: 'Grapes', completed: false },
   { id: 8, name: 'Steak', completed: false },
-  { id: 9, name: 'Salad', completed: false },
+  { id: 9, name: 'Spinach', completed: false },
   { id: 10, name: 'Tea', completed: false }
 ];
 
@@ -37,5 +37,55 @@ function displayGroceries() {
   });
 }
 
-setPageTitle();
-displayGroceries();
+function markComplete(li) {
+  li.classList.add('completed');
+  const iconElement = li.querySelector('i');
+  iconElement.classList.add('completed');
+}
+
+function markIncomplete(li) {
+  li.classList.remove('completed');
+  const iconElement = li.querySelector('i');
+  iconElement.classList.remove('completed');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  setPageTitle();
+  displayGroceries();
+
+
+  //Mark item complete or mark item incomplete
+  const liElements = document.querySelectorAll('li');
+
+  liElements.forEach((element) => {
+    element.addEventListener('click', (event) => { //event.target gives you what was clicked on
+      if (!event.target.classList.contains('completed')) {
+        markComplete(event.target);
+      }
+
+      if (event.detail === 2 && event.target.classList.contains('completed')) {
+        markIncomplete(event.target);
+      }
+    });
+  });
+
+  //Mark all complete or mark all incomplete
+  const toggleAllBtn = document.getElementById('toggleAll');
+
+  toggleAllBtn.addEventListener('click', (event) => {
+    if (allItemsIncomplete) {
+      liElements.forEach((element) => {
+      markComplete(element);
+      toggleAllBtn.innerText = 'Mark All Incomplete';  
+      allItemsIncomplete = false;
+      })
+    } else {
+      liElements.forEach((element) => {
+        markIncomplete(element);
+        toggleAllBtn.innerText = 'Mark All Complete';
+        allItemsIncomplete = true;
+      });
+    }
+  });
+
+});
