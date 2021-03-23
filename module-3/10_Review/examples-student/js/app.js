3430cf342767ce078c0629d490ee39df535766bd
Megan Morris <megan.morris@alumni.stonybrook.edu>
2021-03-23 17:55:50 -0400

    Lecture notes during review

--- module-3/10_Review/examples-student/js/app.js
+++ module-3/10_Review/examples-student/js/app.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
   useHTML.innerHTML=htmlText;
 
   const colorButton = document.getElementById('changeTheColor')
-  colorButton.addEventListener('click', changeColor)
+  colorButton.addEventListener('click', changeColor) //no parens, javascript sends in the event as the parameter
 
   const addAnother = document.getElementById('addLI')
   addAnother.addEventListener('click', () => {
@@ -22,26 +22,28 @@ document.addEventListener('DOMContentLoaded', () => {
     addBtn.addEventListener('click', addNameToList)
   })
 
-  const theList = document.getElementById('theList')
+  const theList = document.getElementById('theList') //event delegation - this way when a new LI element is added the event listener will still catch events because it bubbles up
+  //event delegation - put it on the parent, handle all the clicks of your children
   theList.addEventListener('click', (event) => {
-    if (event.target.innerText == 'Katie') {
+    if (event.target.innerText == 'Katie') { //event.target shows where they clicked
       alert('great answer!')
     } else {
       wtf();
     }
   })
 
+  //propagation
   const propDivs = document.querySelectorAll('div.prop')
   propDivs.forEach((div) => div.addEventListener('click', showPropMsg))
 })
 
 function showPropMsg(event) {
-  let detectedAt = event.target.getAttribute('id')
-  if(detectedAt==null ) {
-    detectedAt =  event.target.parentElement.getAttribute('id')
+  let detectedAt = event.target.getAttribute('id') //event.target is where they clicked
+  if(detectedAt==null ) { //if the click was on the p, get the id from the parent (div)
+    detectedAt =  event.target.parentElement.getAttribute('id') //get the id from the parent element
   }
   
-  const paraToUpdate = event.currentTarget.querySelector('p')
+  const paraToUpdate = event.currentTarget.querySelector('p') 
   paraToUpdate.innerText = paraToUpdate.innerText + 
     ' CLICK DETECTED HERE: ' +
     detectedAt + '!  '
@@ -65,7 +67,7 @@ function wtf() {
 }
 
 function changeColor(event) {
-  let random = Math.random();
+  let random = Math.random(); //returns a random number between 0 and 1
   let colorClass = 'blue';
   if (random < .2) {
     colorClass = 'green';
@@ -75,7 +77,7 @@ function changeColor(event) {
     colorClass = 'purple';
     event.target.removeEventListener('click', changeColor)
     const theText = document.querySelector('div#colorChanger p')
-    theText.innerHTML = 'I like purple. Button is no longer listening.'
+    theText.innerText = 'I like purple. Button is no longer listening.'
     theText.classList.add('bigFont')
   } else if (random < .8) {
     colorClass = 'red';
