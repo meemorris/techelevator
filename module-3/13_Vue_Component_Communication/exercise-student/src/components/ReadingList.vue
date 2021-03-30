22fdbe7d598cf1058cf92af07b6a22c40a6b042d
Megan Morris <megan.morris@alumni.stonybrook.edu>
2021-03-29 23:54:00 -0400

    Vue Component exercise

--- module-3/13_Vue_Component_Communication/exercise-student/src/components/ReadingList.vue
+++ module-3/13_Vue_Component_Communication/exercise-student/src/components/ReadingList.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="book-container">
+    <book-card
+    v-for="bookItem in $store.state.books"
+    v-bind:key="bookItem.title"
+    v-bind:book="bookItem"
+    v-bind:class="{ 'read' : bookItem.read == true}"
+    />
 
   </div>
 </template>
 
 <script>
+import BookCard from './BookCard.vue'
 
 export default {
+  components: { BookCard },
     name: 'reading-list'
 }
 </script>
