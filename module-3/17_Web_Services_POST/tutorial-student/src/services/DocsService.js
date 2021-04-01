f9782c613a0e6ff558951fdf8a74c8cb5575a484
Megan Morris <megan.morris@alumni.stonybrook.edu>
2021-03-31 21:59:21 -0400

    Web Services POST tutorial

--- module-3/17_Web_Services_POST/tutorial-student/src/services/DocsService.js
+++ module-3/17_Web_Services_POST/tutorial-student/src/services/DocsService.js
@@ -12,6 +12,18 @@ export default {
 
   get(id) {
     return http.get(`/docs/${id}`)
+  },
+
+  create(doc) {
+    return http.post(`/docs/`, doc);
+  },
+
+  update(id, doc) {
+    return http.put(`/docs/${id}`, doc);
+  },
+
+  delete(id) {
+    return http.delete(`/docs/${id}`);
   }
 
 }
