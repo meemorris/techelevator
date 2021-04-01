56397e7ca80357f23e989d3c7487487e1b716bc8
Megan Morris <megan.morris@alumni.stonybrook.edu>
2021-03-31 21:05:22 -0400

    Vue Routing tutorial

--- module-3/15_Vue_Routing/tutorial-student/src/App.vue
+++ module-3/15_Vue_Routing/tutorial-student/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <nav>
+       <router-link v-bind:to="{name: 'home'}">Home</router-link> |
+       <router-link v-bind:to="{name: 'portfolio'}">Portfolio</router-link> |
+       <router-link v-bind:to="{name: 'hobby'}">Hobby</router-link>
     </nav>
+    <router-view />
   </div>
 </template>
 
@@ -24,10 +28,11 @@ nav {
 
 nav a {
   font-weight: bold;
-  /* ??? */
+  color: #068488;
 }
 
-/* nav a.????? {
-  
-} */
+nav a.router-link-exact-active {
+  text-decoration: none;
+  color: #e69429;
+}
 </style>
